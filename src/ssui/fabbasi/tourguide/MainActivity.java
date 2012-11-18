22324e1e62d8424be25f78821e2abe6b5156024d
Faiz <faizamodo@gmail.com>
2012-11-18 17:13:22 -0500

    Fixed issue. Mapview now works properly

--- src/ssui/fabbasi/tourguide/MainActivity.java
+++ src/ssui/fabbasi/tourguide/MainActivity.java
@@ -1,63 +1,54 @@
 package ssui.fabbasi.tourguide;
 
-import android.media.MediaPlayer;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+
+import android.location.LocationListener;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.support.v4.app.NavUtils;
 
-public class MainActivity extends Activity {
+public class MainActivity extends MapActivity {
 	
 	//Button to keep track of download click request
-	private Button download;
-	private MediaPlayer mediaPlayer;
+	private Button change;
+	
+	MapController mapController;
+	MapView mapview;
+	LocationListener locationListener;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapview = (MapView)findViewById(R.id.mapview);
+		mapview.setBuiltInZoomControls(true);
+		mapController = mapview.getController();
+	    mapController.setZoom(12);
         
         //Assign the appropriate button
-        download = (Button) findViewById(R.id.button1);
+        change = (Button) findViewById(R.id.button1);
         
         
-        download.setOnClickListener(new View.OnClickListener() {
+        change.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.gangam);
-				if(mediaPlayer != null){
-					mediaPlayer.start();
-				}
+
 				
 			}
 		});
     
     }
     
-    @Override
-    public void onStop(){
-    	super.onStop();
-    	mediaPlayer.release();
-    	mediaPlayer = null;
-    }
-    
-    @Override
-    public void onPause(){
-    	super.onPause();
-    	if(mediaPlayer != null && mediaPlayer.isPlaying()){
-    		mediaPlayer.pause();
-    	}
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
     
 }
