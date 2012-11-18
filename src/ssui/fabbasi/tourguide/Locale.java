dc37232e28227615c9730007916b5ab0065be3fb
Faiz <faizamodo@gmail.com>
2012-11-18 16:40:35 -0500

    Finished baseline implementation of database. Added ACCESS_FINE_LOCATION permission to manifest

--- src/ssui/fabbasi/tourguide/Locale.java
+++ src/ssui/fabbasi/tourguide/Locale.java
@@ -22,7 +22,7 @@ public class Locale {
 	 * @param _latitude An int representation of the latitude of the locale.
 	 * @param _longitude An int representation of the longitude of the locale.
 	 */
-	public Locale(int _id, String _name, String _description, int _latitude, int _longitude){
+	public Locale(int _id, String _name, int _latitude, int _longitude, String _description){
 		id = _id;
 		name = _name;
 		description = _description;
@@ -35,6 +35,35 @@ public class Locale {
 		}
 	}
 	
+
+	/**
+	 * Constructor for a locale object. It takes double values for the latitude and longitude, and converts them into the appropriate int value.
+	 * @param _name The name of this locale
+	 * @param _description The text description that is shown to a user.
+	 * @param _latitude A double representation of the latitude of the locale.
+	 * @param _longitude A double representation of the longitude of the locale.
+	 */
+	public Locale(int _id, String _name, double _latitude, double _longitude, String _description) {
+		id = _id;
+		name = _name;
+		description = _description;
+
+		//Ensure that the coordinates provided are within the appropriate range, otherwise set the point to 0,0.
+		if(_latitude > -90 && _latitude < 90 && _longitude > -180 && _longitude < 180){
+			//If so, convert the double latitude and longitude into equivalent int values, to create a GeoPoint.
+			double lat = _latitude * 1E6;
+			double lon = _longitude * 1E6;
+			
+			int ilat = (int) lat;
+			int ilon = (int) lon;
+			
+			point = new GeoPoint(ilat, ilon);
+		}
+		else{
+			point = new GeoPoint(0,0);
+		}
+	}
+
 	/**
 	 * @return the id
 	 */
--- src/ssui/fabbasi/tourguide/LocaleDataSource.java
+++ src/ssui/fabbasi/tourguide/LocaleDataSource.java
@@ -1,6 +1,10 @@
 package ssui.fabbasi.tourguide;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteStatement;
@@ -47,6 +51,14 @@ public class LocaleDataSource {
 	
 	//Database Manipulations
 	
+	/**
+	 * Insert a new locale into the SQLite database
+	 * @param name The name of the locale
+	 * @param latitude The latitude of the locale (double)
+	 * @param longitude The longitude of the locale (double)
+	 * @param description The description of the locale. To be shown to the user.
+	 * @return
+	 */
 	public long insertLocale(String name, double latitude, double longitude, String description){
 		this.insertStmt.bindString(1, name);
 		this.insertStmt.bindDouble(2, latitude);
@@ -57,8 +69,53 @@ public class LocaleDataSource {
 
 	}
 	
+	/**
+	 * Remove a locale from the database, based on its row ID
+	 * @param rowId the id of the locale
+	 */
 	public void delete(int rowId){
-        database.delete(myHelper.TABLE_NAME, myHelper.COLUMN_ID + "=" + rowId, null); 
+        database.delete(MySQLiteHelper.TABLE_NAME, MySQLiteHelper.COLUMN_ID + "=" + rowId, null); 
+	}
+	
+	/**
+	 * Retrieve all locales in the database, in the order that they were inserted into the database.
+	 * @return
+	 */
+	public List<Locale> getAllLocales(){
+		List<Locale> locales = new ArrayList<Locale>();
+		
+		Cursor cursor = database.query(MySQLiteHelper.TABLE_NAME, allColumns, null, null, null, null, null);
+		
+		cursor.moveToFirst();
+		while(!cursor.isAfterLast()){
+			Locale locale = cursorToLocale(cursor);
+			locales.add(locale);
+			cursor.moveToNext();
+		}
+		
+		return locales;
+	}
+	
+	/**
+	 * Call this method to prepopulate the database with data.
+	 * 
+	 */
+	public void prepopulate(){
+		database.execSQL("INSERT INTO MyTable (name, latitude, longitude, description) " +
+				"VALUES (Empire State Building, 40.7484, 73.9847, The Empire State Building");
+		database.execSQL("INSERT INTO MyTable (name, latitude, longitude, description) " +
+				"VALUES (Golden Gate Bridge, 37.8188, 122.4784, The Golden Gate Bridge is a suspension bridge");
+		database.execSQL("INSERT INTO MyTable (name, latitude, longitude, description) " +
+				"VALUES (Gateway Arch, 38.6254, 90.1834, The Gateway Arch, or Gateway to the West");
+
+	}
+
+
+	private Locale cursorToLocale(Cursor cursor) {
+		Locale locale = new Locale(cursor.getPosition(), cursor.getString(1), cursor.getDouble(2), cursor.getDouble(3), cursor.getString(4));
+		
+		return locale;
+
 	}
 	
 
